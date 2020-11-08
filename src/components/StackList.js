e50d6f55a2f51822ba62060473cbf0b369fd6736
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 12:33:42 -0800

    create stack table not just ul

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux'
 
 const StacksList = ({ stacks }) => {
+  if(stacks){
+    console.log(stacks)
     return (
         <div>
-            <h1>Stacks List:</h1>
-            {stacks.map(stack => 
-                <ul key={stack.id}>
-                    <li >
-                        {stack.attributes.title}
-                    </li>
-                </ul>
+            <h1 className='is-size-1'>Stacks List:</h1>
+            <table className='table is-hoverable is-striped'>
+              <thead>
+                <th>Title</th>
+                <th> # of cards in stack</th>
+              </thead>
+            {stacks.data.map(stack =>
+                <tr key={stack.id}>
+                    <td>{stack.attributes.title}</td>
+                    <td>{stack.relationships.cards.data.length}</td>
+                </tr>
         )}
+        </table>
         </div>
     );
+            }
+            else
+            return (<div></div>)
 };
 
 const mapStateToProps = state => {
