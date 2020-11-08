2d74483501285c7347da0dddc91f9d301e683a56
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 14:31:35 -0800

    add handleChange callback

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Question({front, back}) {
+export default function Question({front, back, handleChange}) {
   return (
     <div>
-      
+      <label>{front}</label>
+      <br />
+      <input type='text' onChange={handleChange} name={front}/>
     </div>
   )
 }
