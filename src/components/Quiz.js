aacc28881a3fef968f3a244e9e23f4eeca691d4f
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 15:19:48 -0800

    add possibility prop

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Question from './Question'
+import Score from './Score'
 
 class Quiz extends Component {
 
   state = {
+    submitted: false,
+    score: 0
   }
 
   handleChange = e => {
@@ -17,25 +20,42 @@ class Quiz extends Component {
 
   handleSubmit = e => {
     e.preventDefault()
-    debugger
-    console.log(this.state)
+    this.setState({submitted: true})
+    const score = this.correctQuiz()
+    console.log(this.correctQuiz())
+    this.setState({score: score})
+  }
+
+  correctQuiz = () => {
+    let score = 0
+    for (const property in this.state) {
+      //console.log(`${property}: ${this.state[property]}`);
+      this.props.stacks.included.forEach( card => {
+        if (card.id === property){
+          if(this.state[property].toLowerCase() === card.attributes.back.toLowerCase()){ score += 1 }
+        }
+      })
+    }
+    return score;
   }
 
   render() {
       return (
       <div  className='has-text-centered'>
         <h2 className='is-size-2'>{this.props.stacks.data[0].attributes.title}</h2>
+        {this.state.submitted ? < Score score={this.state.score} possible={document.querySelectorAll('input').length - 1}/> : null }
         <form onSubmit={this.handleSubmit}>
         {this.props.stacks.included.map( card => {
           if (card.relationships.stack.data.id === this.props.id){
+            //this.setState({possible: this.state.possible + 1})
             return (
             <div key={card.id}>
-              < Question front={card.attributes.front} handleChange={this.handleChange}/>
+              < Question front={card.attributes.front} id={card.id} handleChange={this.handleChange}/>
             </div>
           )
             }
         })}
-        <input type='submit' value='Submit Quiz'/>
+        <input type='submit' value='Submit Quiz' className='button is-primary' />
         </form>
       </div>
       )
