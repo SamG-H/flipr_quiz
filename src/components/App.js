26e4fc75471ca09c20fe9c3a0085e6987860d5cd
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 12:32:41 -0800

    add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import StackContainer from './StackContainer'
+import Router from './Router'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        < StackContainer />
+        < Router />
       </div>
     )
   }
