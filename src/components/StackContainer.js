5f5bdbd94b0771064a624f26ae7ea05f4569cd05
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 12:49:41 -0800

    add callback handleClick

--- src/components/StackContainer.js
+++ src/components/StackContainer.js
@@ -7,11 +7,16 @@ import {fetchStacks} from '../actions/stacksActions'
 class StackContainer extends Component {
   componentDidMount() {
     this.props.fetchStacks()
-}
+  }
+
+  handleClick = e => {
+    console.log(e.target.id)
+  }
+
   render() {
     return (
       <div>
-        < StackList />
+        < StackList handleClick={this.handleClick}/>
       </div>
     )
   }
