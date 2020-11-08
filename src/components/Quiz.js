5efa8e5e209e1c48a19cbb96aa0ff8922831f5ec
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 14:32:10 -0800

    clean up and use Question component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,22 +3,40 @@ import { connect } from 'react-redux'
 import Question from './Question'
 
 class Quiz extends Component {
+
+  state = {
+  }
+
+  handleChange = e => {
+    const {name, value} = e.target
+    //const answers = this.state.answers
+    this.setState({
+      [name] : value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    debugger
+    console.log(this.state)
+  }
+
   render() {
-    console.log(this.props.stacks.data[0].attributes.title)
       return (
-      <div>
-        {this.props.id}
-        {this.props.stacks.data[0].attributes.title}
+      <div  className='has-text-centered'>
+        <h2 className='is-size-2'>{this.props.stacks.data[0].attributes.title}</h2>
+        <form onSubmit={this.handleSubmit}>
         {this.props.stacks.included.map( card => {
           if (card.relationships.stack.data.id === this.props.id){
             return (
             <div key={card.id}>
-              <p>Front: {card.attributes.front}</p>
-              <p>Back: {card.attributes.back}</p>
+              < Question front={card.attributes.front} handleChange={this.handleChange}/>
             </div>
           )
             }
         })}
+        <input type='submit' value='Submit Quiz'/>
+        </form>
       </div>
       )
   }
