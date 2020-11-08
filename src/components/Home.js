37bc6e367bdcb97016a0c699678643541328597b
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 12:49:12 -0800

    add text

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <h1 className='is-size-1'>
-      Welcome to Flipr Quiz
+      Welcome to Flipr Quiz - Test Your Knowledge
     </h1>
   )
 }
