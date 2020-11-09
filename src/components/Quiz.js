b8c902711ae27731f0f3497165e15c7bb987dbed
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 17:22:10 -0800

    fix state structure

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,17 +5,25 @@ import Score from './Score'
 
 class Quiz extends Component {
 
+  componentDidMount() {
+    //this.props.resetStackId()
+  }
+
   state = {
     submitted: false,
-    score: 0
+    score: 0,
+    answers: {}
   }
 
   handleChange = e => {
     const {name, value} = e.target
     //const answers = this.state.answers
-    this.setState({
-      [name] : value
-    })
+    let answers = {...this.state.answers}
+    answers[name] = {
+      content: value,
+      isCorrect: false
+    }
+    this.setState({answers})
   }
 
   handleSubmit = e => {
@@ -27,12 +35,16 @@ class Quiz extends Component {
   }
 
   correctQuiz = () => {
+    console.log('state', this.state)
     let score = 0
-    for (const property in this.state) {
-      //console.log(`${property}: ${this.state[property]}`);
+    for (const property in this.state.answers) {
+      console.log('property:', property)
       this.props.stacks.included.forEach( card => {
         if (card.id === property){
-          if(this.state[property].toLowerCase() === card.attributes.back.toLowerCase()){ score += 1 }
+          //if(this.state.answers[property].toLowerCase() === card.attributes.back.toLowerCase()){ 
+          //  score += 1
+            
+          //}
         }
       })
     }
@@ -42,19 +54,19 @@ class Quiz extends Component {
   render() {
       return (
       <div  className='has-text-centered'>
-        <h2 className='is-size-2'>{this.props.stacks.data[0].attributes.title}</h2>
+        <h2 className='is-size-2'>{this.props.title}</h2>
         {this.state.submitted ? < Score score={this.state.score} possible={document.querySelectorAll('input').length - 1}/> : null }
         <form onSubmit={this.handleSubmit}>
         {this.props.stacks.included.map( card => {
           if (card.relationships.stack.data.id === this.props.id){
-            //this.setState({possible: this.state.possible + 1})
             return (
             <div key={card.id}>
-              < Question front={card.attributes.front} id={card.id} handleChange={this.handleChange}/>
+              < Question front={card.attributes.front} id={card.id} result={'is-danger'} handleChange={this.handleChange}/>
             </div>
           )
             }
         })}
+        <br />
         <input type='submit' value='Submit Quiz' className='button is-primary' />
         </form>
       </div>
