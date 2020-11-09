adf3466d6e1b8ccc91b1803242fb37135bf6bf89
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 18:10:38 -0800

    remove if clause and use callback to configure color

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function Question({front, id, handleChange, result}) {
-  return (
-    <div className='field'>
-      <label className='label is-size-5'>{front}</label>
-      <div class="control">
-      <input type='text' onChange={handleChange} name={id} className={'input ' + result}  style={{ width: '30%' }}/>
+export default function Question({front, id, handleChange, configureColor}) {
+    return (
+      <div className='field'>
+        <label className='label is-size-5'>{front}</label>
+        <div class="control">
+        <input type='text' onChange={handleChange} name={id} className='input'  style={{ width: '30%' }}/>
+        </div>
       </div>
-    </div>
-  )
+    )
 }
