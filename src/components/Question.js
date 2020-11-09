b8bcc16f0a8f8fd2359fd11d809a21108dc7577a
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 17:21:40 -0800

    add width property to input

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Question({front, id, handleChange}) {
+export default function Question({front, id, handleChange, result}) {
   return (
     <div className='field'>
-      <label className='label'>{front}</label>
+      <label className='label is-size-5'>{front}</label>
       <div class="control">
-      <input type='text' onChange={handleChange} name={id} className='input'/>
+      <input type='text' onChange={handleChange} name={id} className={'input ' + result}  style={{ width: '30%' }}/>
       </div>
     </div>
   )
