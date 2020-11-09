9e41e8870a69f164fdf73a4168d6c8c7a084642a
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-08 17:24:01 -0800

    change list of stacks to buttons

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -7,9 +7,11 @@ const StacksList = ({ stacks, handleClick }) => {
         <div>
             <h1 className='is-size-1'>Stacks List:</h1>
             {stacks.data.map(stack =>
-              <p key={stack.id} id={stack.id} onClick={handleClick} className='is-size-5'>
-                {stack.attributes.title} - {stack.relationships.cards.data.length}
-              </p>
+            <div>
+              <button key={stack.id} id={stack.id} onClick={handleClick} className='is-size-3 button is-link is-light'>
+                {stack.attributes.title}
+              </button>
+              </div>
         )}
         </div>
     );
