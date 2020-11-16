c84bd3caf18e4868272cba12b4c6064c01430e5a
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-16 12:46:21 -0800

    make stack title a navlink

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import { NavLink, Route } from 'react-router-dom';
+import Home from './Home'
 
 export default class Stack extends Component {
   render() {
-    console.log(this.props.title)
     return (
-      <p>
-        {this.props.title}
-      </p>
+      <>
+        <NavLink to={'/stacks/' + this.props.id} exact className='is-size-2 is-link'>{this.props.title}</NavLink><br />
+      </>
     )
   }
 }
