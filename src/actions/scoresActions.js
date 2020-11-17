5d123ed8e30e01bbce138fca6c6997cf88757f1c
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-17 13:15:03 -0800

    add createScore action

--- src/actions/scoresActions.js
+++ src/actions/scoresActions.js
@@ -5,3 +5,19 @@ export const fetchScores = () => {
       .then(scores => dispatch({ type: 'FETCH_SCORES', payload: scores}))
   }
 }
+
+export const createScore = (score) => {
+  return dispatch => {
+    fetch(`http://localhost:3000/scores/`, {
+      method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+    },
+    body: JSON.stringify(score)
+  })
+      .then(resp => resp.json())
+      .then(score => {(console.log(score))})
+    }
+  }
+
