bb3cb6128c6927c517078764813d68bde24086bc
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-11-16 18:23:47 -0800

    add styling depending on score

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default function Score({score, possible}) {
+  let className = ''
+  if(possible > score){
+    className=' has-text-danger'
+  } else {
+    className=' has-text-success'
+  }
   return (
     <div>
-      <h1 className='is-size-2 has-text-link'>You Scored {score}/{possible}</h1>
+      <p className={'is-size-2' + className}>You Scored {score}/{possible}</p>
     </div>
   )
 }
