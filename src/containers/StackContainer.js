f80e5be99a943edc9ce6ea4905dba941d111b637
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-12-10 11:20:43 -0800

    add header to stacks route

--- src/containers/StackContainer.js
+++ src/containers/StackContainer.js
@@ -5,6 +5,7 @@ export default class StackContainer extends Component {
   render() {
     return (
       <div className='has-text-centered'>
+        <h1 className='is-size-1'>Review Stacks</h1>
         <Stacks />
       </div>
     )
