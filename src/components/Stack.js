c89ca14da43f4d0efe2aef17edc07398a8e08610
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-12-10 11:01:14 -0800

    fix indentation

--- src/components/Stack.js
+++ src/components/Stack.js
@@ -16,16 +16,18 @@ class Stack extends Component {
     return (
       <div className='has-text-centered'>
         <h1 className='is-size-2 has-text-link'>{this.props.cards.included[0].attributes.title}</h1>
-        {this.props.cards.data.map( card => {
-            return (
-              <Card
-                front={card.attributes.front} 
-                back={card.attributes.back} 
-                id={card.id} 
-                key={card.id}
-              />
+        <div className='cardsDiv'>
+          {this.props.cards.data.map( card => {
+              return (
+                <Card
+                  front={card.attributes.front}
+                  back={card.attributes.back}
+                  id={card.id}
+                  key={card.id}
+                />
+            )}
           )}
-        )}
+        </div>
       </div>
     )
   }
