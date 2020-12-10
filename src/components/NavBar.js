708cd527cb6baf7c725e370dea23878e3d329388
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-12-10 10:39:02 -0800

    add stacks navlink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,7 @@ export default class NavBar extends Component {
         <NavLink to="/" exact className='navbar-item'>Home</NavLink>
         <NavLink to="/about" exact className='navbar-item'>About</NavLink>
         <NavLink to="/quizzes" exact className='navbar-item'>Quizzes</NavLink>
+        <NavLink to="/stacks" exact className='navbar-item'>Stacks</NavLink>
       </nav>
     )
   }
