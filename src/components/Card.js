ef75f600b3b1ba5f4b996221066536fed5012e8f
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-12-10 11:19:11 -0800

    add toggle for front and back of cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react'
 import '../Card.css'
 
 export default class Card extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      isToggleOn: true
+    };
+  }
+
+  handleClick = () => {
+    this.setState(state => (
+      {
+        isToggleOn: !state.isToggleOn
+      }
+    ));
+  }
+
   render() {
     return (
-      <div>
-        <div className='card'>{this.props.front}</div>
-        <div className='card' style={{display: 'none'}}>{this.props.back}</div>
+      <div onClick={this.handleClick}>
+        {this.state.isToggleOn ? <div className='card'>{this.props.front}</div> : <div className='card clicked'>{this.props.back}</div>}
       </div>
     )
   }
