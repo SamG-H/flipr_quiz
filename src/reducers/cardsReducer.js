3822c364850b30966684836977274aeb6f87a857
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-03-04 09:22:46 -0800

    fix ADD_CARD and ADD_STACK in reducers

--- src/reducers/cardsReducer.js
+++ src/reducers/cardsReducer.js
@@ -1,10 +1,13 @@
 export const cardsReducer = (state = [], action) => {
-  switch(action.type){
-      case 'FETCH_CARDS':
-        return action.payload
-      case 'ADD_CARD':
-        return action.payload
-      default:
-        return state
+  switch (action.type) {
+    case "FETCH_CARDS":
+      return action.payload;
+    case "ADD_CARD":
+      return {
+        ...state,
+        data: [...state.data, action.payload.data],
+      };
+    default:
+      return state;
   }
-}
+};
--- src/reducers/stacksReducer.js
+++ src/reducers/stacksReducer.js
@@ -1,10 +1,13 @@
 export const stacksReducer = (state = [], action) => {
-  switch(action.type){
-      case 'FETCH_STACKS':
-        return action.payload
-      case 'ADD_STACK':
-        return action.payload
-      default:
-        return state
+  switch (action.type) {
+    case "FETCH_STACKS":
+      return action.payload;
+    case "ADD_STACK":
+      return {
+        ...state,
+        data: [...state.data, action.payload.data],
+      };
+    default:
+      return state;
   }
-}
+};
