cd1e4355f1b1f571d70cdc0ced91acba04d87e1b
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2021-03-21 13:11:46 -0700

    reformat fetchCards action

--- src/actions/cardsActions.js
+++ src/actions/cardsActions.js
@@ -2,7 +2,9 @@ export const fetchCards = (id) => {
   return (dispatch) => {
     fetch(`http://localhost:3000/stacks/${id}/cards`)
       .then((resp) => resp.json())
-      .then((cards) => dispatch({ type: "FETCH_CARDS", payload: cards }));
+      .then((cards) => {
+        dispatch({ type: "FETCH_CARDS", payload: cards });
+      });
   };
 };
 
